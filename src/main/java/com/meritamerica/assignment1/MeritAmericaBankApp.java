5086063951b5f9cfab075c3f59280709590782fb
Shelby <71155281+SWHurdyGurdyMan@users.noreply.github.com>
2020-10-04 17:27:48 -0500

    Add files via upload
    Updated BankApp file

--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -1,5 +1,28 @@
 package com.meritamerica.assignment1;
 
 public class MeritAmericaBankApp {
-	
+	public static void main(String[] args){
+        AccountHolder accountHolder = new AccountHolder(
+            	"Sadiq",
+            	"",
+            	"Manji",
+            	"123456789",
+            	100,
+            	1000);
+        System.out.println(accountHolder.toString());
+        SavingsAccount.deposit(500);
+        SavingsAccount.withdraw(800);
+        System.out.println(checkingAccount.toString());
+        System.out.println(savingsAccount.toString());
+        AccountHolder newAccountHolder = new AccountHolder(
+            	"Sadiq",
+            	"",
+            	"Manji",
+            	"123456789",
+            	200,
+            	500);
+        CheckingAccount.deposit(-500);
+        SavingsAccount.withdraw(600);
+        System.out.println(newAccountHolder.toString());
+	}
 }
