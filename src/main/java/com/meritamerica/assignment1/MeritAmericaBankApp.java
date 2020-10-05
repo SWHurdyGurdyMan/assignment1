605731c007a9b784ba3a7a956d1a6d0c0e0ea2ce
Shelby <71155281+SWHurdyGurdyMan@users.noreply.github.com>
2020-10-05 12:56:34 -0500

    Add files via upload

--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -1,28 +1,39 @@
 package com.meritamerica.assignment1;
 
+//string out everything into the banking app between two parties
+
+
 public class MeritAmericaBankApp {
-	public static void main(String[] args){
-        AccountHolder accountHolder = new AccountHolder(
-            	"Sadiq",
-            	"",
-            	"Manji",
-            	"123456789",
-            	100,
-            	1000);
-        System.out.println(accountHolder.toString());
-        SavingsAccount.deposit(500);
-        SavingsAccount.withdraw(800);
-        System.out.println(checkingAccount.toString());
-        System.out.println(savingsAccount.toString());
-        AccountHolder newAccountHolder = new AccountHolder(
-            	"Sadiq",
-            	"",
-            	"Manji",
-            	"123456789",
-            	200,
-            	500);
-        CheckingAccount.deposit(-500);
-        SavingsAccount.withdraw(600);
-        System.out.println(newAccountHolder.toString());
+	public static void main(String[] args) {
+		
+		//Dore
+		AccountHolder DoreM = new AccountHolder(
+				"Dore",
+				"",
+				"Mendoza",
+				"999999999",
+				100.0,
+				1000.0);
+		System.out.println(DoreM.toString());
+		DoreM.getCheckingAccount().deposit(500);
+		DoreM.getSavingsAccount().withdraw(800);
+		System.out.println(DoreM.getCheckingAccount().toString());
+		System.out.println(DoreM.getSavingsAccount().toString());
+		
+		//Shelby
+		AccountHolder ShelbyW = new AccountHolder(
+				"Shelby",
+				"Eugene",
+				"Walles",
+				"123456789",
+				200.0,
+				500.0);
+		System.out.println(ShelbyW.toString());
+		ShelbyW.getCheckingAccount().deposit(-500);
+		ShelbyW.getSavingsAccount().withdraw(600);
+		System.out.println(ShelbyW.toString());
+		System.out.println(ShelbyW.getCheckingAccount().toString());
+		System.out.println(ShelbyW.getSavingsAccount().toString());
 	}
+
 }
